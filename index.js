d2ad823b433bae989b086dc6f4aa5946f2bfa20b
Yukti Gupta <yuktigupta95@gmail.com>
2026-05-04 23:14:10 +0530

    Add modal messages

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-/*
-Challenge:
-1. Take control of the close button.
-2. Use an event listener to set the display 
-   property of the modal to 'none' when the
-   close button is clicked.
-*/
+const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
 
-const modal = document.getElementById('modal')
-const modalCloseBtn = document.getElementById('modal-close-btn')
- 
-setTimeout(function(){
-    modal.style.display = 'inline'
-}, 1500)
+setTimeout(function () {
+  modal.style.display = "inline";
+}, 1500);
 
-modalCloseBtn.addEventListener('click', function(){
-    modal.style.display = 'none'
-})
+modalCloseBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+consentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>`;
+
+  setTimeout(function () {
+    document.getElementById("upload-text").innerText = `Making the sale...`;
+  }, 1500);
+
+  setTimeout(function () {
+    document.getElementById("modal-inner").innerHTML =
+      `<h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>`;
+  }, 3000);
+});
